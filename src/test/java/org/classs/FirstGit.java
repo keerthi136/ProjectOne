160fb1b3ca74ba7286d0ec03e687a9f7f33f3cf5
keerthi136 <keerthanaece.498@gmail.com>
2022-11-06 18:17:45 +0530

    First Git Hub

---
+++ src/test/java/org/classs/FirstGit.java
@@ -0,0 +1,18 @@
+package org.classs;
+
+public class FirstGit {
+  
+	public void firstClass() {
+System.out.println("1");
+	}
+	public void secondClass() {
+System.out.println("2");
+	}
+	public void thirdClass() {
+System.out.println("3");
+	}
+	
+	
+	
+	
+}
